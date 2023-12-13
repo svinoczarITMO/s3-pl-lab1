87e4c17553375f03d6429c556a83a8b39acc7169
svinoczar <alexsupper75@gmail.com>
2023-12-13 22:05:30 +0300

    image rota

--- solution/include/RotatePicture.h
+++ solution/include/RotatePicture.h
@@ -10,6 +10,19 @@ struct image{
     struct pixel* data;
 };
 
+enum read_status {
+    READ_OK = 0,
+    READ_INVALID_SIGNATURE,
+    READ_INVALID_BITS,
+    READ_INVALID_HEADER
+};
+
+enum write_status {
+    WRITE_OK = 0,
+    WRITE_ERROR
+
+};
+
 struct image rotate(struct image source);
 
 #endif
