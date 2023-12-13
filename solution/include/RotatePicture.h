6576d4fd185734e217e12597508e0b10c4351b86
svinoczar <alexsupper75@gmail.com>
2023-12-13 23:21:24 +0300

    image rotation rotati

--- solution/include/RotatePicture.h
+++ solution/include/RotatePicture.h
@@ -3,15 +3,6 @@
 
 #include <stdint.h>
 
-struct pixel{
-    uint8_t b, g, r;
-};
-
-struct image{
-    uint16_t width, height;
-    struct pixel* data;
-};
-
 struct image rotate(struct image source);
 
 #endif
