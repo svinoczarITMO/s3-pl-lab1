c6c299c51b79485b227aa07fa5c9f22a3776bff7
svinoczar <alexsupper75@gmail.com>
2023-12-13 22:07:25 +0300

    image rotat

--- solution/src/RotatePicture.c
+++ solution/src/RotatePicture.c
@@ -1,14 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-struct pixel{
-    uint8_t b, g, r;
-};
-
-struct image{
-    uint16_t width, height;
-    struct pixel* data;
-};
 
 struct image rotate(struct image source){
     if (source.data != NULL){
